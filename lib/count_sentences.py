c2b12079c6a08cea2ba1c9af5da3758f151e1e62
Nijhan <najmashaaban6@gmail.com>
2025-08-14 00:03:59 +0300

    initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # use setter for validation
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.') if self.value else False
+
+    def is_question(self):
+        return self.value.endswith('?') if self.value else False
+
+    def is_exclamation(self):
+        return self.value.endswith('!') if self.value else False
+
+    def count_sentences(self):
+        # Match one or more sentence-ending punctuation marks as one sentence
+        return len(re.findall(r'[.!?]+', self.value))
